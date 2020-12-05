b2c56ff90e2bedf82ac064d60069b4450c320b78
Dhaval Patel <dhavalpatel244@gmail.com>
2020-12-05 23:36:03 +0530

    Make splash screens functional

--- app/src/main/java/com/github/dhaval2404/itunesplayer/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/github/dhaval2404/itunesplayer/ui/splash/SplashActivity.kt
@@ -1,8 +1,10 @@
 package com.github.dhaval2404.itunesplayer.ui.splash
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.github.dhaval2404.itunesplayer.R
+import com.github.dhaval2404.itunesplayer.databinding.ActivitySplashBinding
+import com.github.dhaval2404.itunesplayer.ui.base.BaseActivity
 
 /**
  * Splash Screen
@@ -11,11 +13,34 @@ import com.github.dhaval2404.itunesplayer.R
  * @version 1.0
  * @since 05 December 2020
  */
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : BaseActivity<ActivitySplashBinding, SplashViewModel>() {
+
+    override fun getLayout() = R.layout.activity_splash
+
+    override fun getViewModel(): SplashViewModel {
+        return ViewModelProvider(this).get(SplashViewModel::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        mViewDataBinding.viewModel = mViewModel
+
+        setObserver()
+
+        // Perform Initialization
+        mViewModel.preProcessing()
     }
 
+    /**
+     * Listen to ViewModel changes
+     */
+    private fun setObserver() {
+        mViewModel.syncLiveData.observe(this, {
+            startSongActivity()
+        })
+    }
+
+    private fun startSongActivity() {
+
+    }
 }
--- app/src/main/java/com/github/dhaval2404/itunesplayer/ui/splash/SplashViewModel.kt
+++ app/src/main/java/com/github/dhaval2404/itunesplayer/ui/splash/SplashViewModel.kt
@@ -1,9 +1,11 @@
 package com.github.dhaval2404.itunesplayer.ui.splash
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModel
-import com.github.dhaval2404.itunesplayer.R
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.github.dhaval2404.itunesplayer.ui.base.BaseViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 /**
  * ViewModel for SplashActivity
@@ -12,6 +14,26 @@ import com.github.dhaval2404.itunesplayer.R
  * @version 1.0
  * @since 05 December 2020
  */
-class SplashViewModel : ViewModel() {
+class SplashViewModel : BaseViewModel() {
 
+    private val mSyncLiveData = MutableLiveData<Boolean>()
+    val syncLiveData: LiveData<Boolean> = mSyncLiveData
+
+    /**
+     * Perform task on App Launch
+     */
+    fun preProcessing() {
+        viewModelScope.launch {
+            // Add dummy delay
+            delay(SPLASH_DELAY)
+
+            // Start Next Screen
+            mSyncLiveData.postValue(true)
+        }
+    }
+
+    companion object {
+        // 3 sec delay
+        private const val SPLASH_DELAY = 3000L
+    }
 }
